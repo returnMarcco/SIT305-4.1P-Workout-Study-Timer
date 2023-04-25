eef8e37d32751f16a0f4a0f8a15a17a690b58f83
jvellucci <jvellucci@deakin.edu.au>
2023-04-25 20:09:12 +1000

    finish UI design/layout for workout timer application

---
+++ 41PWorkoutTimer/app/src/main/java/com/example/a41p_workout_timer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a41p_workout_timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
