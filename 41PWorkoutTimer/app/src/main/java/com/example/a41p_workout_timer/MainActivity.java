3bbaf6d6be0c9f663b9046f0832ded8aa0f17854
jvellucci <jvellucci@deakin.edu.au>
2023-05-06 22:08:05 +1000

    add error-handling logic & resetAllTimers() void method

--- 41PWorkoutTimer/app/src/main/java/com/example/a41p_workout_timer/MainActivity.java
+++ 41PWorkoutTimer/app/src/main/java/com/example/a41p_workout_timer/MainActivity.java
@@ -52,43 +52,60 @@ public class MainActivity extends AppCompatActivity {
         };
     }
     protected void triggerWorkoutTimer() { // Event-Handler
+        // Timer start/pause flag
+        boolean timerRunning = false;
+
         try {
-            EditText workoutDurationInput = findViewById(R.id.idWorkoutDurationEditText);
-            String workoutDurationStr = workoutDurationInput.getText().toString();
+            if (timerRunning = false) {
+                timerRunning = true;
 
-            if (workoutDurationStr.isEmpty()) {
-                workoutDurationStr = "1";
-                System.out.println(workoutDurationStr);
+                EditText workoutDurationInput = findViewById(R.id.idWorkoutDurationEditText);
+                String workoutDurationStr = workoutDurationInput.getText().toString();
+
+                if (workoutDurationStr.isEmpty()) {
+                    workoutDurationStr = "1";
+                    System.out.println(workoutDurationStr);
+                }
+
+
+                int workoutDuration = Integer.parseInt(workoutDurationStr);
+                System.out.println(workoutDuration);
+
+                EditText restPeriodDurationInput = findViewById(R.id.idRestPeriodDurationEditText);
+                String restPeriodDurationStr = restPeriodDurationInput.getText().toString();
+
+                if (restPeriodDurationStr.isEmpty()) {
+                    restPeriodDurationStr = "1";
+                }
+
+                int restPeriodDuration = Integer.parseInt(restPeriodDurationStr);
+                System.out.println(restPeriodDuration);
+                EditText numberOfSetsInput = findViewById(R.id.idSetSelector);
+                String numberOfSetsStr = numberOfSetsInput.getText().toString();
+
+                if (numberOfSetsStr.isEmpty()) {
+                    numberOfSetsStr = "1";
+                }
+
+                int numberOfSets = Integer.parseInt(numberOfSetsStr);
+                System.out.println(numberOfSets);
+
+//              CountDownTimer workoutDurationTimer = createWorkoutTimer(workoutDuration, numberOfSets);
+//              CountDownTimer restPeriodTimer = createRestPeriodTimer(restPeriodDuration);
+
+
+            } else {
+                // When the timer is running and the start/pause button is pressed again
             }
-
-
-            int workoutDuration = Integer.parseInt(workoutDurationStr);
-            System.out.println(workoutDuration);
-
-            EditText restPeriodDurationInput = findViewById(R.id.idRestPeriodDurationEditText);
-            String restPeriodDurationStr = restPeriodDurationInput.getText().toString();
-
-            if (restPeriodDurationStr.isEmpty()) {
-                restPeriodDurationStr = "1";
-            }
-
-            int restPeriodDuration = Integer.parseInt(restPeriodDurationStr);
-            System.out.println(restPeriodDuration);
-            EditText numberOfSetsInput = findViewById(R.id.idSetSelector);
-            String numberOfSetsStr = numberOfSetsInput.getText().toString();
-
-            if (numberOfSetsStr.isEmpty()) {
-                numberOfSetsStr = "1";
-            }
-
-            int numberOfSets = Integer.parseInt(numberOfSetsStr);
-            System.out.println(numberOfSets);
-
-//            CountDownTimer workoutDurationTimer = createWorkoutTimer(workoutDuration, numberOfSets);
-//            CountDownTimer restPeriodTimer = createRestPeriodTimer(restPeriodDuration);
         } catch(Error e) {
             Log.e("triggerWorkoutTimer: ", "An error has occurred");
         }
     }
+
+    protected void resetAllTimers() {
+        // Button that resets all timers
+    }
+
+
 
 }
